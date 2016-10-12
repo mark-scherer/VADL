54aefaec902d97c156723c9a2cd07926bf124b22
Mark Scherer <mark.s.scherer@vanderbilt.edu>
2016-10-11 22:33:00 -0500

    Again12

--- js/trim_pics.js
+++ js/trim_pics.js
@@ -11,7 +11,7 @@ $(function() {
 	/* select all divs with child images of class .trimmed-pic */
 	for (var i = 0; i < divs.length; i++)
 	{
-		console.log(divs[i].childNodes[0].classList.length);
+		console.log(divs[i].childNodes.length);
 		if (divs[i].childNodes[0].length > 0)
 		{
 			if (divs[i].childNodes[0].classList.contains("trimmed-pic"))

404a277dacf3349e307ba928adc683c5a5bb0d60
Mark Scherer <mark.s.scherer@vanderbilt.edu>
2016-10-11 21:53:12 -0500

    Real JS test

---
+++ js/test.js
@@ -0,0 +1,7 @@
+$(function() {
+	var tester = document.getElementByType('link');
+	for (var i = 0; i < tester.length; i++)
+	{
+		tester[i].style.fontSize = '200px';
+	}	
+})
---
+++ js/trim_pics.js
@@ -0,0 +1,49 @@
+/* resizes all .trimmed-pic images to fill window based upon aspect ratios */
+
+$(function() {
+	var trimmedPicsParents[];
+	var tooTall[];
+	var tooWide[];
+
+	/* select all .trimmed-pic images */
+	/* first select all divs */
+	var divs = document.getElementById("div");
+	/* select all divs with child images of class .trimmed-pic */
+	for (var i = 0; i < divs.length; i++)
+	{
+		if (divs[i].childNodes[0].className === "trimmed-pic")
+		{
+			trimmedPicsParents.push(divs[i]);
+		}
+	}
+
+	/* sort into too-tall and too-wide */
+	for (var i = 0; i < trimmedPicsParents.length; i++)
+	{
+		/* find window and image aspect ratio */
+		var windowAspect = i.clientWidth / i.clientHeight;
+		var imgAspect = i.childNodes[0].clientWidth / i.childNodes[0].clientHeight;
+		/* add to correct list */
+		if (imgAspect > windowAspect)
+		{
+			tooTall.push(i.childNodes[0]);
+		}
+		if (imgAspect < windowAspect)
+		{
+			tooWide.push(i.childNodes[0]);
+		}
+	}
+
+	/* resize appropriately */
+	for (var i = 0; i < tooTall.length; i++)
+	{
+		tooTall[i].style.maxWidth = 100%;
+		tooTall[i].style.minHeight = 100%;
+	}
+	for (var i = 0; i < tooWide.length; i++)
+	{
+		tooWide[i].style.maxHeight = 100%;
+		tooWide[i].style.minWidth = 100%;
+	}
+}
+)

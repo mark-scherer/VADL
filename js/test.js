845be61beb9dbd095887d7bab59c0c0780795d9e
Mark Scherer <mark.s.scherer@vanderbilt.edu>
2016-10-11 21:57:20 -0500

    and again

--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 $(function() {
-	var tester = document.getElementByTagName('h2');
+	var tester = document.getElementsByTagName('h2');
 	for (var i = 0; i < tester.length; i++)
 	{
 		tester[i].style.fontSize = '200px';
